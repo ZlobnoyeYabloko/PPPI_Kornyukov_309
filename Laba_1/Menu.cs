c32e74bdc69eda78079450665af466515464e699
ZlobnoyeYabloko <73900550+ZlobnoyeYabloko@users.noreply.github.com>
2023-02-11 00:04:01 +0200

    Delete Laba_1 directory

--- Laba_1/Menu.cs
+++
@@ -1,6 +0,0 @@
-﻿using Laba_1;
-
-Console.WriteLine("0 - Text words counter,  1 - Calculator");
-Console.WriteLine("Choose case: ");
-var cases = new Menu(Convert.ToInt32(Console.ReadLine()));
-cases.MenuChoise();
