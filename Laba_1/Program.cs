457c949c7314a73473f102e3d661301b275ffa1a
ZlobnoyeYabloko <73900550+ZlobnoyeYabloko@users.noreply.github.com>
2023-02-11 00:05:16 +0200

    Add files via upload

---
+++ Laba_1/Program.cs
@@ -0,0 +1,6 @@
+﻿using Laba_1;
+
+Console.WriteLine("0 - Text words counter,  1 - Calculator");
+Console.WriteLine("Choose case: ");
+var cases = new Menu(Convert.ToInt32(Console.ReadLine()));
+cases.MenuChoise();
